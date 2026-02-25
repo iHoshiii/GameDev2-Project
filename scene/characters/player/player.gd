ba5398ac9d6f5e43f13410ac844ba383e9ba62eb
iHoshiii <aldwinloreto690@gmail.com>
2026-02-25 13:28:19 +0800

    Initial commit

---
+++ scene/characters/player/player.gd
@@ -0,0 +1,10 @@
+class_name Player
+extends CharacterBody2D
+
+@export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+
+var player_direction: Vector2
+
+
+func _on_chopping_ready() -> void:
+	pass # Replace with function body.
